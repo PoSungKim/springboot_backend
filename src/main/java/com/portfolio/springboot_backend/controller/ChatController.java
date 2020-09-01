dd314f288ae0a3875c4cd87f169b9d9073e99fab
PoSungKim <psungkr94@likelion.org>
2020-09-01 14:40:49 +0900

    Controller에서 Path 별로 처리를 다르게 하여 다음 내용 처리 ==> 오픈 채팅방에 들어왔을 시 혹은 나갈 시에 관련 채팅봇 메시지 (ex) 환영 메세지

--- src/main/java/com/portfolio/springboot_backend/controller/ChatController.java
+++ src/main/java/com/portfolio/springboot_backend/controller/ChatController.java
@@ -14,11 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class ChatController {
 
     // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/messaging/handler/annotation/MessageMapping.html
-    @MessageMapping("/mychat.register") // @Payload는 Default 값으로 설정된다고 함
-    @SendTo("/topic/public")   // /topic/public 구독자/참여자들에게 chatMessage를 BroadCast함
-    public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) throws Exception {
-        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        System.out.println("/mychat.register");
+    @MessageMapping("/join") // @Payload는 Default 값으로 설정된다고 함
+    @SendTo("/topic/public")   // 참여할 시 환영 메세지
+    public ChatMessage joinMessage(@Payload ChatMessage chatMessage) throws Exception {
+        System.out.println("/mychat.join" + chatMessage);
+        chatMessage.setContent( (chatMessage.getSender().equals("손님") ? "손님 " : chatMessage.getSender() + "님 ") + "안녕하세요! 오픈 채팅방에 오신 것을 환영합니다 ^^");
+        chatMessage.setSender("오픈챗 봇");
         return chatMessage;
     }
 
@@ -26,6 +27,15 @@ public class ChatController {
     @SendTo("/topic/public")
     public ChatMessage sendMessage(@Payload ChatMessage chatMessage) throws Exception {
         System.out.println("/mychat.send " + chatMessage);
+        return chatMessage;
+    }
+
+    @MessageMapping("/leave") // @Payload는 Default 값으로 설정된다고 함
+    @SendTo("/topic/public")   // 방을 나갈 시 메세지
+    public ChatMessage leaveMessage(@Payload ChatMessage chatMessage) throws Exception {
+        System.out.println("/mychat.leave" + chatMessage);
+        chatMessage.setContent( (chatMessage.getSender().equals("손님") ? "손님" : chatMessage.getSender() + "님") + "께서 방을 나가셨습니다 ^^");
+        chatMessage.setSender("오픈챗 봇");
         return chatMessage;
     }
 
